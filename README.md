642eb363d85af6ae5659a9670cebefb72465dc70
Nicolas <millt.nico@hotmail.fr>
2017-09-08 01:00:10 +0200

    Adding description & bootstrap
    Adding description (not fully fonctionnal) & bootstrap

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-.checkout
+Portfolio of Nicolas MYLLE
 =========
+Feel free to use or contact me for any question.
 
 A Symfony project created on July 25, 2017, 5:11 pm.
---
+++ web/assets/js/custom.js
@@ -0,0 +1,9 @@
+jQuery(document).ready(function($) {
+    console.log( "ready!" );
+
+    /*function collapseDescription(elementToOpen) {
+        jQuery('.collapse.in').collapse('hide');
+        jQuery(elementToOpen).collapse("toggle");
+    }*/
+});
+
